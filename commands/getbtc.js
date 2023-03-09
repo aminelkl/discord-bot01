ac20c066bdd29e7f7bb67ecc9f89e0a82c90397b
amine <aminelakhalamine@hotmail.fr>
2023-03-08 22:52:00 -0500

    Added getbtc slash commands

--- commands/getbtc.js
+++ commands/getbtc.js
@@ -1,11 +1,40 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const axios = require('axios');
+const { cmcApi } = require('../config.json');
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('getbtc')
 		.setDescription('Replies with Bitcoin price!'),
         
 	async execute(interaction) {
-		await interaction.reply('');
+		let price = null;
+
+		let response = null;
+		new Promise(async (resolve, reject) => {
+			try {
+			  response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=BTC&convert=USD', {
+				headers: {
+				  'X-CMC_PRO_API_KEY': cmcApi,
+				},
+			  });
+			} catch(ex) {
+			  response = null;
+			  // error
+			  console.log(ex);
+			  reject(ex);
+			}
+			if (response) {
+			  // success
+			  const json = response.data.data;
+			  console.log(json.BTC[0].quote.USD.price);
+			  resolve(json);
+			}
+				price = json.BTC[0].quote.USD.price;
+		  });
+
+
+		await interaction.reply(price);
 	},
 };
